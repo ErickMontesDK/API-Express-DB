9cfdaf309c51f71eb5bcaac3edd46e29c68b626a
Erick Montes <erickmontesdk@gmail.com>
2022-05-05 00:26:49 -0500

    agregado CRUD segunda tabla

--- server.js
+++ server.js
@@ -16,10 +16,21 @@ app.get('/explorers', async (req, res) => {
     res.json(allExplorers);
   });
 
+  app.get('/commanders', async (req, res) => {
+    const allCommanders =  await prisma.commander.findMany({});
+    res.json(allCommanders);
+  });
+
 app.get('/explorers/:id', async (req, res) => {
   const id = req.params.id;
   const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
   res.json(explorer);
+});
+
+app.get('/commanders/:id', async (req, res) => {
+  const id = req.params.id;
+  const commander = await prisma.commander.findUnique({where: {id: parseInt(id)}});
+  res.json(commander);
 });
 
 app.post('/explorers', async (req, res) => {
@@ -30,6 +41,18 @@ app.post('/explorers', async (req, res) => {
      };
     const message = 'Explorer creado.';
     await prisma.explorer.create({data: explorer});
+    return res.json({message});
+  });
+
+  app.post('/commanders', async (req, res) => {
+    const commander = {
+      name: req.body.name,
+      lang: req.body.username,
+      missionCommander: req.body.mission,
+      enrollments:req.body.enrollments
+     };
+    const message = 'Commander creado.';
+    await prisma.commander.create({data: commander});
     return res.json({message});
   });
 
@@ -47,9 +70,29 @@ app.put('/explorers/:id', async (req, res) => {
 	return res.json({message: "Actualizado correctamente"});
 });
 
+app.put('/commanders/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+
+	await prisma.commander.update({
+		where: {
+			id: id
+		},
+		data: {
+			missionCommander: req.body.mission
+		}
+	})
+	return res.json({message: "Actualizado correctamente"});
+});
+
 app.delete('/explorers/:id', async (req, res) => {
 	const id = parseInt(req.params.id);
 	await prisma.explorer.delete({where: {id: id}});
+	return res.json({message: "Eliminado correctamente"});
+});
+
+app.delete('/commanders/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+	await prisma.commander.delete({where: {id: id}});
 	return res.json({message: "Eliminado correctamente"});
 });
 
